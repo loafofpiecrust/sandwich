42462760d6b280b9629008609825684a86218fc6
Taylor Snead <taylorsnead@gmail.com>
2020-05-13 15:43:57 -0400

    :construction: Switch to rodio for audio synthesis

--- src/audio.rs
+++ src/audio.rs
@@ -1,43 +1,22 @@
 use crate::grammar;
 use anyhow;
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{self, Sample};
 use itertools::Itertools;
+use rodio::{self, Sink};
 use std::{f32, thread, time::Duration};
 
 pub fn play_sound(frequencies: (f32, f32), duration: Duration) -> anyhow::Result<()> {
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("No device available");
-    let config = device.default_output_config()?;
+    let device = rodio::default_output_device().unwrap();
+    let sink1 = Sink::new(&device);
+    let sink2 = Sink::new(&device);
 
-    let sample_rate = config.sample_rate().0 as f32;
-    let channels = config.channels() as usize;
-
-    // Produce a sin-wave!
-    let mut sample_clock = 0.0;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * frequencies.0 * f32::consts::PI / sample_rate).sin()
-            + (sample_clock * frequencies.1 * f32::consts::PI / sample_rate).sin()
-    };
-
-    let err_fn = |err| eprintln!("{}", err);
-
-    let stream = device.build_output_stream(
-        &config.into(),
-        move |data: &mut [f32], _| {
-            for frame in data.chunks_mut(channels) {
-                let value = Sample::from(&next_value());
-                for sample in frame.iter_mut() {
-                    *sample = value;
-                }
-            }
-        },
-        err_fn,
-    )?;
-    stream.play()?;
+    // Play two sine waves at once for dual-tone effect.
+    sink1.append(rodio::source::SineWave::new(frequencies.0 as u32));
+    sink2.append(rodio::source::SineWave::new(frequencies.1 as u32));
 
     thread::sleep(duration);
+
+    sink1.stop();
+    sink2.stop();
 
     Ok(())
 }
@@ -47,7 +26,7 @@ pub fn play_word(word: &str) -> anyhow::Result<()> {
     for (a, b) in word.chars().tuples() {
         play_sound(
             (consonant_sound(a), vowel_sound(b)),
-            Duration::from_millis(150),
+            Duration::from_millis(200),
         )?;
     }
     Ok(())
