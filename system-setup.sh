6ec74c47d45ef2398425fb64c6d82b306b9ad6a4
Taylor Snead <taylorsnead@gmail.com>
2020-06-27 00:04:52 -0400

    :construction: Fix setup sudo

--- system-setup.sh
+++ system-setup.sh
@@ -7,19 +7,19 @@
 touch /boot/ssh
 
 # Set the hostname.
-echo "$1" > /etc/hostname
-sed -i "s/raspberrypi/$1/g" /etc/hosts
+echo "$1" | sudo tee -a /etc/hostname
+sudo sed -i "s/raspberrypi/$1/g" /etc/hosts
 
 # Advertise this machine to the local network.
-apt-get install avahi-daemon
+sudo apt-get install avahi-daemon
 
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup default stable
 
 # Project dependencies
-apt-get install git
-apt-get install x11-xserver-utils
-apt-get install nvim
+sudo apt-get install git
+sudo apt-get install x11-xserver-utils
+sudo apt-get install vim
 
 # Now we'll need a reboot.
