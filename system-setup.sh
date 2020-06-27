fc63e776fa7bafc1194c6b8d4237ddb2d51f38ad
Taylor Snead <taylorsnead@gmail.com>
2020-06-26 22:16:55 -0400

    :sparkles: Add system setup script for pi 3b+

---
+++ system-setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Connect to the shared router.
+wpa_passphrase "NETGEAR48" "$2" > /etc/wpa_supplicant/wpa_supplicant.conf
+
+# Enable ssh.
+touch /boot/ssh
+
+# Set the hostname.
+echo "$1" > /etc/hostname
+sed -i "s/raspberrypi/$1/g" /etc/hosts
+
+# Advertise this machine to the local network.
+apt-get install avahi-daemon
+
+# Install rust
+apt-get install rustup
+rustup default stable
+
+# Project dependencies
+apt-get install git
+apt-get install xrandr
+apt-get install x11-xserver-utils
+
+# Now we'll need a reboot.
